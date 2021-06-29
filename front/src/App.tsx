4534f7454677a55af4044e149093815f159a63e7
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-06-29 11:01:13 +0900

    TypeScript用に環境構築（papaparse）

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import Papa from 'papaparse'
 import Router from './Router'
 import { Header } from './components/header'
 
 const App = () => {
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://www3.nhk.or.jp/n-data/opendata/coronavirus/nhk_news_covid19_prefectures_daily_data.csv'
+      )
+      .then((response) => {
+        // console.log('response', response.data);
+        // console.log('response', response.data);
+        const JsonCorona = Papa.parse(response.data, {
+          header: true,
+          dynamicTyping: true,
+          encoding: 'Shift-JIS',
+          // transformHeader: function (header, index) {
+          //   console.log('transformHeader', header, index);
+          // },
+          // エラーを取り除く
+          // skipEmptyLines: true,
+          // データを解析
+          // complete: function (results, file) {
+          //   console.log('Parsing complete:', results, file);
+          // },
+          // step: function (row) {
+          //   console.log(row.data);
+          // },
+          // transformHeader: true,
+        });
+        console.log('JsonCorona', JsonCorona.data);
+      });
+  });
   return (
     <>
       <Header />
