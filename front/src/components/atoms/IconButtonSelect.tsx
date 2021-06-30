dda38849e304352e51138f4730567dcee8c4b2ce
masaokaka <osushiiisensu@gmail.com>
2021-06-30 23:50:13 +0900

    ヘッダーとドロワー編集

--- front/src/components/atoms/IconButtonSelect.tsx
+++ front/src/components/atoms/IconButtonSelect.tsx
@@ -11,26 +11,47 @@ import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import EditIcon from "@material-ui/icons/Edit";
 import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
+import TimelineIcon from "@material-ui/icons/Timeline";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import MultilineChartIcon from "@material-ui/icons/MultilineChart";
+import ShowChartIcon from "@material-ui/icons/ShowChart";
+
+export type icon =
+  | "Menu"
+  | "Admin"
+  | "Cart"
+  | "History"
+  | "Login"
+  | "Logout"
+  | "Search"
+  | "Arrow"
+  | "Edit"
+  | "List"
+  | "NewAccount"
+  | "BarGrph1"
+  | "BarGrph2"
+  | "LineGrph1"
+  | "LineGrph2"
+  | "LineGrph3"
+  | "CircleGrph"
+  | string;
 interface Props {
-  icon:
-    | "Menu"
-    | "Admin"
-    | "Cart"
-    | "History"
-    | "Login"
-    | "Logout"
-    | "Search"
-    | "Arrow"
-    | "Edit"
-    | "List"
-    | "NewAccount";
+  icon: icon;
   color?: "inherit" | "primary" | "secondary" | "action" | "disabled" | "error";
-  onClick: () => void;
+  onClick?: () => void;
+  disabled?: boolean;
 }
 
-export const IconButtonSelect: FC<Props> = ({ icon, color, onClick }) => {
+export const IconButtonSelect: FC<Props> = ({
+  icon,
+  color,
+  onClick,
+  disabled,
+}) => {
   return (
-    <IconButton onClick={onClick}>
+    <IconButton onClick={onClick} disabled={disabled}>
       {icon === "Menu" && <MenuIcon color={color} />}
       {icon === "Admin" && <SupervisorAccountIcon color={color} />}
       {icon === "Cart" && <ShoppingCartIcon color={color} />}
@@ -42,6 +63,12 @@ export const IconButtonSelect: FC<Props> = ({ icon, color, onClick }) => {
       {icon === "Edit" && <EditIcon color={color} />}
       {icon === "List" && <FormatListBulletedIcon color={color} />}
       {icon === "NewAccount" && <PersonAddIcon color={color} />}
+      {icon === "BarGrph1" && <EqualizerIcon color={color} />}
+      {icon === "BarGrph2" && <BarChartIcon color={color} />}
+      {icon === "LineGrph1" && <TimelineIcon color={color} />}
+      {icon === "LineGrph2" && <MultilineChartIcon color={color} />}
+      {icon === "LineGrph3" && <ShowChartIcon color={color} />}
+      {icon === "CircleGrph" && <DonutLargeIcon color={color} />}
     </IconButton>
   );
 };
--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -1,40 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { Container } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
 import { useAppSelector } from "../../app/hooks";
 import { useHistory } from "react-router";
 import { logout } from "../../features/user/userAPI";
 import { selectUser } from "../../features/user/userSlice";
 import { IconButtonSelect } from "../atoms/IconButtonSelect";
+import ClosableDrawer from "./ClosableDrawer";
+import { icon } from "../atoms/IconButtonSelect";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     grow: {
-      flexGrow: 1,
+      flexGrow: 0.5,
     },
     mr20: {
       marginRight: 10,
     },
     menu: {
-      [theme.breakpoints.down("xs")]: {
+      [theme.breakpoints.down("sm")]: {
+        display: "none",
+      },
+    },
+    graphs: {
+      [theme.breakpoints.down("sm")]: {
         display: "none",
       },
     },
     drawer: {
-      [theme.breakpoints.up("sm")]: {
+      [theme.breakpoints.up("md")]: {
         display: "none",
       },
     },
   })
 );
 
+export interface headerItems {
+  text: string;
+  icon: icon;
+  method: () => void;
+}
+
 const Header: React.FC = () => {
   const classes = useStyles();
   const history = useHistory();
   const userData = useAppSelector(selectUser);
+  const [toggle, setToggle] = useState(false);
+  const headers = {
+    logins: [
+      { text: "掲示板", icon: "List", method: () => history.push("/threads") },
+      { text: "ログアウト", icon: "Logout", method: () => logout() },
+    ],
+    logouts: [
+      { text: "ログイン", icon: "Login", method: () => history.push("/login") },
+      {
+        text: "新規登録",
+        icon: "NewAccount",
+        method: () => history.push("/register"),
+      },
+    ],
+    graphs: [
+      { text: "グラフ1", icon: "LineGrph1", method: () => history.push("/") },
+      { text: "グラフ2", icon: "LineGrph2", method: () => history.push("/") },
+      { text: "グラフ3", icon: "LineGrph3", method: () => history.push("/") },
+      { text: "グラフ4", icon: "CircleGrph", method: () => history.push("/") },
+      { text: "グラフ5", icon: "BarGrph1", method: () => history.push("/") },
+      { text: "グラフ6", icon: "BarGrph2", method: () => history.push("/") },
+    ],
+  };
   return (
     <div className={classes.grow}>
       <AppBar position="static">
@@ -44,35 +80,62 @@ const Header: React.FC = () => {
               ラクラクコロナ
             </Typography>
             <div className={classes.grow} />
+            <>
+              {headers.graphs.map((graph, index) => (
+                <div
+                  className={classes.graphs}
+                  key={index}
+                  style={{ marginRight: 30 }}
+                >
+                  <Grid spacing={2}>
+                    <Grid>
+                      <IconButtonSelect
+                        icon={graph.icon}
+                        onClick={graph.method}
+                      />
+                    </Grid>
+                    <Grid>
+                      <small>{graph.text}</small>
+                    </Grid>
+                  </Grid>
+                </div>
+              ))}
+            </>
+            <div className={classes.grow} />
             {userData ? (
               <>
-                <Typography>こんにちは {userData?.username} さん</Typography>
-                <IconButtonSelect
-                  icon={"List"}
-                  onClick={() => history.push("/threads")}
-                />
-                <IconButtonSelect
-                  icon={"Logout"}
-                  onClick={() => {
-                    logout();
-                  }}
-                />
+                <Typography> {userData?.username} さん</Typography>
+                {headers.logins.map((login, index) => (
+                  <div className={classes.menu} key={index}>
+                    <IconButtonSelect
+                      icon={login.icon}
+                      onClick={login.method}
+                    />
+                  </div>
+                ))}
               </>
             ) : (
               <>
-                <IconButtonSelect
-                  icon={"NewAccount"}
-                  onClick={() => history.push("/register")}
-                />
-                <IconButtonSelect
-                  icon={"Login"}
-                  onClick={() => history.push("/login")}
-                />
+                {headers.logouts.map((logout, index) => (
+                  <div className={classes.menu} key={index}>
+                    <IconButtonSelect
+                      icon={logout.icon}
+                      onClick={logout.method}
+                    />
+                  </div>
+                ))}
               </>
             )}
+            <div className={classes.drawer}>
+              <IconButtonSelect
+                icon={"Menu"}
+                onClick={() => setToggle(!toggle)}
+              />
+            </div>
           </Toolbar>
         </Container>
       </AppBar>
+      <ClosableDrawer toggle={toggle} setToggle={setToggle} headers={headers} />
     </div>
   );
 };
