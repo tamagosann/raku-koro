112f0f53fa8cbdd3bda30830e9d7358fe6d37c2f
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-01 11:08:28 +0900

    円グラフのサイズ変換と%の単位をつけ、実装完了

--- front/src/templates/BedOccupancyRate.tsx
+++ front/src/templates/BedOccupancyRate.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppSelector } from '../app/hooks';
 import Inner from '../components/inner/Inner';
 
 //react-modal
 import Modal from 'react-modal';
+import { PieChart, Pie, Cell, Tooltip } from 'recharts';
 
 // slice
 import { selectBedOccupancyRate } from '../features/graphs/bedOccupancyRateSlice';
@@ -44,12 +45,39 @@ const useStyles = makeStyles((theme: Theme) =>
       color: theme.palette.text.secondary,
       backgroundColor: '#ff2b2b',
     },
-    backgroundColorStageOne: {},
   })
 );
 
-const ModalPreview = (props: any) => {
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+const COLORS = ['#bdc3c7', '#ff2b2b'];
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}: any): JSX.Element => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? 'start' : 'end'}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+const ModalPreview = ({ element, customStyles, data, useBedRateing }: any) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   function openModal() {
     setIsOpen(true);
@@ -62,34 +90,53 @@ const ModalPreview = (props: any) => {
   return (
     <>
       <div onClick={openModal}>
-        <h3>{props.element.prefecture}</h3>
-        <p>{props.element.use_bed_rate}</p>
+        <h3>{element.prefecture}</h3>
+        <p>{element.use_bed_rate}</p>
         <p>
-          {props.element.inpatient} / {props.element.secure_bed}
+          {element.inpatient} / {element.secure_bed}
         </p>
       </div>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        style={props.customStyles}
+        style={customStyles}
         contentLabel="Example Modal"
       >
         <h2>
-          {props.element.prefecture} 病床使用率{' '}
-          {((props.element.inpatient / props.element.secure_bed) * 100).toFixed(
-            2
-          )}{' '}
-          % (参考)
+          {element.prefecture} 病床使用率{' '}
+          {((element.inpatient / element.secure_bed) * 100).toFixed(2)} % (参考)
         </h2>
-
-        {/* ここに円グラフ入れてください！ */}
-
-        <p>PCR検査陽性者数：{props.element.pcr_positive}人</p>
-        <p>うち重症者数：{props.element.injured}人</p>
-        <p>入院患者受入確保病床：{props.element.secure_bed}床</p>
-        <p>入院者数：{props.element.inpatient}人</p>
-        <p>出典：{props.element.source}</p>
-        <p>最終更新日：{props.element.update}</p>
+        <PieChart width={400} height={400} style={{ margin: '0 auto' }}>
+          <Pie
+            startAngle={-270}
+            data={data}
+            cy={200}
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius="80%"
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry: any, index: number) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          {/* Tooltipに表示される単位を選定 */}
+          <Tooltip
+            formatter={(value: number) => {
+              return `${value}%`;
+            }}
+          />
+        </PieChart>
+        <p>PCR検査陽性者数：{element.pcr_positive}人</p>
+        <p>うち重症者数：{element.injured}人</p>
+        <p>入院患者受入確保病床：{element.secure_bed}床</p>
+        <p>入院者数：{element.inpatient}人</p>
+        <p>出典：{element.source}</p>
+        <p>最終更新日：{element.update}</p>
       </Modal>
     </>
   );
@@ -118,6 +165,20 @@ export const BedOccupancyRate = () => {
           <div className={classes.root}>
             <Grid container spacing={3}>
               {BedOccupancyRates.data.map((element, index) => {
+                const data = [
+                  {
+                    name: '病床空き率',
+                    value: Math.round(
+                      100 - (element.inpatient / element.secure_bed) * 100
+                    ),
+                  },
+                  {
+                    name: '病床使用率',
+                    value: Math.round(
+                      (element.inpatient / element.secure_bed) * 100
+                    ),
+                  },
+                ];
                 const useBedRateing =
                   (Number(element.inpatient) / Number(element.secure_bed)) *
                   100;
@@ -135,6 +196,8 @@ export const BedOccupancyRate = () => {
                       }
                     >
                       <ModalPreview
+                        data={data}
+                        useBedRateing={useBedRateing}
                         customStyles={customStyles}
                         element={element}
                       />
