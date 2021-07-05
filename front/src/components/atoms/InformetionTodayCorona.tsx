6751988d470ac2610967a9c0ec441f2412ca2a55
AtsuyaSakamoto <sakamo.070501@gmail.com>
2021-07-05 12:11:06 +0900

    クリエイティブコモンズページへの遷移リンクを追加

--- front/src/components/atoms/InformetionTodayCorona.tsx
+++ front/src/components/atoms/InformetionTodayCorona.tsx
@@ -11,7 +11,7 @@ const InformetionTodayCorona: FC = () => {
     <p style={{ textAlign: 'center', fontSize: '24px' }}>
       感染者数 更新日：{coronaHuman[coronaIndex].date}
       <br />
-      ※対策病床使用率(参考)=現在患者 / 新型コロナ対策病床数
+      ※対策病床使用率(参考) = 現在患者 / 新型コロナ対策病床数
       <br />
       <a
         href='https://corona.go.jp/dashboard/'
@@ -22,14 +22,27 @@ const InformetionTodayCorona: FC = () => {
       </a>
       を使用
       <br />
-      <a
-        href='https://www.stopcovid19.jp/'
-        target='_blunk'
-        rel='noopener noreferrer'
-      >
-        新型コロナウイルス対策ダッシュボード
-      </a>
-      を使用
+      <span>
+        DATA:{' '}
+        <a
+          href='https://creativecommons.org/licenses/by/4.0/deed.ja'
+          target='_blunk'
+          rel='noopener noreferrer'
+        >
+          CC BY
+        </a>
+      </span>
+      <span> </span>
+      <span>
+        <a
+          href='https://www.stopcovid19.jp/'
+          target='_blunk'
+          rel='noopener noreferrer'
+        >
+          新型コロナウイルス対策ダッシュボード
+        </a>
+        を使用
+      </span>
     </p>
   );
 };
