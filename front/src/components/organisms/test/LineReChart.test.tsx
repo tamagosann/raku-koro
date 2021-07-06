6c15d839be00987116082d391fa1b6746dffd9dd
daikiku10 <soccerdk19.910@gmail.com>
2021-07-06 09:31:48 +0900

    LineReChart仮データ作成

--- front/src/components/organisms/test/LineReChart.test.tsx
+++ front/src/components/organisms/test/LineReChart.test.tsx
@@ -1,6 +1,21 @@
 import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+interface Data {
+  date: string;
+  npatients: number;
+  adpatients: number;
+}
+
+interface Props {
+  data: Data[];
+  xDataKey: string;
+  lineDataKey: string;
+  lineName: string;
+  startIndex: number;
+  endIndex: number;
+}
+
 describe('日別グラフチャートのテスト', () => {
     // テストケースを実行する前にやって欲しい処理
     beforeEach(() => {
@@ -10,6 +25,23 @@ describe('日別グラフチャートのテスト', () => {
     afterEach(() => {
       cleanup();
     });
+
+    test('テスト', () => {
+      const LineReChartProps: Props = {
+        data: [{
+          date: "2021-07-05",
+          npatients: 600,
+          adpatients: 400
+        }
+        ],
+        xDataKey: "date",
+        lineDataKey: "npatients",
+        lineName: "感染者数",
+        startIndex: 1,
+        endIndex: 30
+      };
+      
+    })
 
     
 })
