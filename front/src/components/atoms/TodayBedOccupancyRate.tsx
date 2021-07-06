9c9f623a800a3d729b6ac41cdd26d2dab237b656
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-06 20:42:54 +0900

    処理ロジック切り離してテスト実装

--- front/src/components/atoms/TodayBedOccupancyRate.tsx
+++ front/src/components/atoms/TodayBedOccupancyRate.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, FC } from 'react';
+import React, { FC } from 'react';
 import { GraphState } from '../../features/graphs/bedOccupancyRateSlice';
 interface Data {
   pcr_positive: number;
@@ -18,34 +18,25 @@ interface Props {
   bedOccupancyRate: GraphState;
 }
 
-const calcSumBed = (bedOccupancyRate: GraphState) => {
+const calcBedUsedRate = (bedOccupancyRate: GraphState) => {
   // 対策病床使用率を算出
   // 入院者数*入院患者受入確保病床=入院患者病床使用率
   let sumBed: number = 0;
   bedOccupancyRate.data.forEach((todayBed: Data) => {
     sumBed += todayBed.secure_bed;
   });
-  return sumBed;
-};
-const calcSumInpatient = (bedOccupancyRate: GraphState) => {
+
   let sumInpatient: number = 0;
   bedOccupancyRate.data.forEach((todayInpatient: Data) => {
     sumInpatient += todayInpatient.inpatient;
   });
-  return sumInpatient;
+
+  let totalBedUsed: string = ((sumInpatient / sumBed) * 100).toFixed(2);
+  return totalBedUsed;
 };
 
 const TodayBedOccupancyRate: FC<Props> = ({ bedOccupancyRate }) => {
-  const [bedUsed, setBedUsed] = useState(bedOccupancyRate.data);
-
-  useEffect(() => {
-    setBedUsed(bedOccupancyRate.data);
-  }, [bedOccupancyRate]);
-
-  const sumBed = calcSumBed(bedOccupancyRate);
-  const sumInpatient = calcSumInpatient(bedOccupancyRate);
-  let totalBedUsed: string = ((sumInpatient / sumBed) * 100).toFixed(2);
-
+  const totalBedUsed = calcBedUsedRate(bedOccupancyRate);
   return (
     <>
       <h2 style={{ fontSize: '24px' }}>対策病床使用率(参考)※</h2>
