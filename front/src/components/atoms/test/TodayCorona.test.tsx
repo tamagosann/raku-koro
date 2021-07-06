40cdbe3ca5f00935357c4eaf0d52c3505a4bb71c
AtsuyaSakamoto <sakamo.070501@gmail.com>
2021-07-06 09:45:56 +0900

    atomsの単体テストを実施

--- front/src/components/atoms/test/TodayCorona.test.tsx
+++ front/src/components/atoms/test/TodayCorona.test.tsx
@@ -1,11 +1,11 @@
-// import React from 'react';
+import React from 'react';
 import { cleanup, render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-import TodayCorona from './TodayCorona';
+import userEvent from '@testing-library/user-event';
+import TodayCorona from '../TodayCorona';
 import { Provider, useSelector } from 'react-redux';
-import { useAppSelector } from '../../app/hooks';
-import { store } from '../../app/store';
-import { selectTotalCorona } from '../../features/graphs/totalCoronaSlice';
+import { useAppSelector } from '../../../app/hooks';
+import { store } from '../../../app/store';
+import { selectTotalCorona } from '../../../features/graphs/totalCoronaSlice';
 
 // const mockFunk = jest.fn();
 
---
+++ front/src/components/atoms/test/TodayDeth.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import TodayDeth from '../TodayDeth';
+import { Provider, useSelector } from 'react-redux';
+import { useAppSelector } from '../../../app/hooks';
+import { store } from '../../../app/store';
+
+describe(`TodayDeth`, () => {
+  test(`renders TodayDeth`, () => {
+    render(
+      <Provider store={store}>
+        <TodayDeth />
+      </Provider>
+    );
+    screen.getByRole('');
+    // screen.debug();
+  });
+});
