e5c0180eb37f9f4ef33a29c5f596c0eda80cbb81
masaokaka <osushiiisensu@gmail.com>
2021-07-07 15:44:12 +0900

    ファイル名も変更

--- front/src/templates/informationCorona.tsx
+++ front/src/templates/informationCorona.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react";
+import React, { FC } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
