39bd6ab3e3209d9ebfe68c1d88a0d7b1a1fb0b2b
AtsuyaSakamoto <sakamo.070501@gmail.com>
2021-07-07 13:41:19 +0900

    pull

--- front/src/templates/informationCorona.tsx
+++ front/src/templates/informationCorona.tsx
@@ -64,7 +64,7 @@ export const InformationCorona: FC = () => {
                 </Grid>
                 <Grid item xs={12} md={6}>
                   <Paper className={classes.paper}>
-                    <TodayDeth totalCorona={totalDeth}  />
+                    <TodayDeth totalCorona={totalDeth} />
                   </Paper>
                 </Grid>
                 <Grid item xs={12} md={6}>
